5b63d96922e3b422e36144c43abd266ebda5a8a8
dandrusco <41076761+dandrusco@users.noreply.github.com>
2023-07-30 15:23:39 -0400

    App 1 completada

--- 01-First_app/First_app/ContentView.swift
+++ 01-First_app/First_app/ContentView.swift
@@ -10,13 +10,18 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hola, Mundo!")
+            Text("Tectroya es una empresa especializada en el área de la informática. Nuestro trabajo se basa en interpretar las necesidades de nuestros clientes, identificar las oportunidades y desarrollar tecnologías innovadoras.")
                 .fontWeight(.bold)
                 .font(.system(.largeTitle, design: .rounded))//Con system se adapta el texto a la pantalla del iPhone
-                .foregroundColor(.purple)
+                .foregroundColor(.orange)
+                .multilineTextAlignment(.center)
+                .lineLimit(nil)//Cuantas linas quieres mostrar, por defacto es Nil
+                .truncationMode(.tail)//Pone los ... al final
+                .lineSpacing(8)//es para ancho entre lineas
+                .padding()//Para darle mas espaco en los bordes del iPhone
+                //.rotationEffect(.degrees(45))//Para rotar textos
+                .rotation3DEffect(.degrees(50), axis: (x: 1, y: 0, z: 0))//Rotar texto en 3D
+                .shadow(color: .gray, radius: 1, x: 0, y: 3)//Para dar sombra
         }
         .padding()
     }
