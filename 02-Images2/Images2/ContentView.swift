75d016e9fb37266836bc4919590b7a48032ead97
dandrusco <41076761+dandrusco@users.noreply.github.com>
2023-07-31 16:52:45 -0400

    Uso de imagenes en SwiftUI completado

--- 02-Images2/Images2/ContentView.swift
+++ 02-Images2/Images2/ContentView.swift
@@ -10,11 +10,30 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
+            //Image("fondoNuevo").imageScale(.large).foregroundColor(.accentColor)//Forma actual de agregar imagen por defecto
+            
             //Image("fondoNuevo").resizable() //Pone una imagen en una zona segura
             
             //Image("fondoNuevo").scaledToFit()//Hace que la imagen no pierda zona de la imagen
             
-            Image("fondoNuevo").imageScale(.large).foregroundColor(.accentColor)//Forma actual de agregar imagen por defecto
+            //Image("fondoNuevo").resizable().clipShape(Circle())//Crea la imagen circular
+            
+            Image("fondoNuevo")
+                .resizable()
+                .clipShape(Circle())
+                .opacity(0.8)//Modifica la opacidad
+                .overlay(Image(systemName: "heart.fill")
+                    .font(.system(size: 120))
+                    .foregroundColor(.pink))//Overay es como otra vista
+                .overlay(Text("SwiftUI")
+                    .fontWeight(.bold))
+            //Podemos oscurecer la imagen con sus capas (overlay)
+                .overlay(Circle()
+                    .foregroundColor(.gray)
+                    .opacity(0.5)
+                    .overlay(Text("Tectroya")
+                        .font(.title)
+                        .fontWeight(.bold),alignment: .top))
         }
         .padding()
     }
