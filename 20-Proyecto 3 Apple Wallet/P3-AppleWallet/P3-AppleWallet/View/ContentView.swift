84b2d39abf287d3ce3f9b9ddec6eccd4d4dbd0c0
dandrusco <41076761+dandrusco@users.noreply.github.com>
2023-10-31 16:50:05 -0300

    Crear el trarjetero de tarjetas de creditos

--- 20-Proyecto 3 Apple Wallet/P3-AppleWallet/P3-AppleWallet/View/ContentView.swift
+++ 20-Proyecto 3 Apple Wallet/P3-AppleWallet/P3-AppleWallet/View/ContentView.swift
@@ -8,17 +8,98 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    //para conseguir el abanico de cartas
+    private let offset: CGFloat = 75.0
+    
+    //Modificamos el ofset para que cada una tenga mas offset que el anterior
+    private func offset(for card: Card)->CGSize{
+        //Creamos un indice de la tarjeta actual
+        guard let idx = cards.firstIndex(where: {$0.id == card.id}) else {
+            return CGSize() //(0,0)
+        }
+        //Obtenemos la pocision real del desplazamiento
+        return CGSize(width: 0, height: -offset * CGFloat(idx))
+    }
+    
+    //Invertiremos el otrden de las tarjetas
+    private func zIndex(for card: Card) -> Double{
+        guard let idx = cards.firstIndex(where: {$0.id == card.id}) else {
+            return 0.0
+        }
+        return -Double(idx)
+    }
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            TopBar()
+            Spacer()
+            ZStack{
+                ForEach(cards){ c in
+                    CardView(card: c)
+                        .offset(offset(for: c))
+                        .padding(.horizontal, 20)
+                        .zIndex(zIndex(for: c))
+                        .shadow(color: .gray, radius: 1.5, x: 1.0, y: 1.0)
+                    //TODO: girar el orden de las tarjetas zIndex
+                }
+            }
+            Spacer()
         }
-        .padding()
+    }
+}
+
+struct CardView: View {
+    var card: Card
+    
+    var body: some View {
+        Image(card.image)
+            .resizable()
+            .scaledToFit()
+            .overlay(
+                VStack (alignment: .leading){
+                    Text(card.number)
+                        .bold()
+                    
+                    Text(card.clientName)
+                        .bold()
+                    
+                    HStack {
+                        Text("Valida hasta:")
+                            .font(.caption)
+                        Text(card.expirationDate)
+                            .font(.caption)
+                    }
+                    
+                }
+                    .foregroundStyle(.white)
+                    .padding(.leading, 55)
+                    .padding(.bottom, 45)
+                    .shadow(color: .black, radius: 1.5, x: 0.0, y: 1.0)
+                , alignment: .bottomLeading
+            )
     }
 }
 
 #Preview {
     ContentView()
+    //CardView(card: cards[0]).previewLayout(.sizeThatFits)
+}
+
+
+struct TopBar: View {
+    var body: some View {
+        HStack {
+            Text("Cartera")
+                .font(.system(.largeTitle, design: .rounded))
+                .fontWeight(.heavy)
+            
+            Spacer()
+            
+            Image(systemName: "plus.circle")
+                .font(.system(.largeTitle))
+        }
+        .padding(.horizontal, 20)
+        .padding(.vertical, 30)
+    }
 }
